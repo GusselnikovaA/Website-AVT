6de10229598479e6457994ca0623c364ab359f03
GusselnikovaA <annanac1993@gmail.com>
2019-10-08 14:53:52 +0300

    Настройка и подключение слайдера

--- js/premises-slider.js
+++ js/premises-slider.js
@@ -1,24 +1,9 @@
 // СЛАЙДЕР В БЛОКЕ PREMISES
 $(document).ready(function () {
-  $('.premises-slide').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-        // infinite: true,
-        // speed: 2000,
-        // prevArrow: $('.hero-arrows__left'),
-        // nextArrow: $('.hero-arrows__right'),
-        // responsive: [{
-        //     breakpoint: 890,
-        //     settings: {
-        //       arrows: false,
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 414,
-        //     settings: "unslick"
-        //   }
-        // ]
-      });
+  $('.premises-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.premises__arrows__left'),
+    nextArrow: $('.premises__arrows__right'),
+  });
 });
