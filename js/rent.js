11549574f24514749310c7e255f7b04de4a9ebe7
GusselnikovaA <annanac1993@gmail.com>
2019-10-21 15:45:31 +0300

    Обложки для помещений в слайдере. Схемы помещений.

--- js/rent.js
+++ js/rent.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
-  let premise = $('#premise');
+  let premise = $('.premise');
   let rent = $('#rent');
   let close = $('#close');
-  // let price = $('.card__link');
+  let rentLink = $('.rent__link');
 
   premise.on('click', function(){
-    rent.addClass('modal_active');
+    rent.addClass('rent_active');
+  });
+
+  premise.on('click', function(){
+    rent.addClass('rent_active');
   });
   // price.on('click', function(){
   //   modal.addClass('modal_active');
   // });
   
   close.on('click', function(){
-    rent.removeClass('modal_active');
+    rent.removeClass('rent_active');
   });
 
 
