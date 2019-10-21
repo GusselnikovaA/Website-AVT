393109b8e2db069f8cc55e3e888fcc2c3adaf2eb
GusselnikovaA <annanac1993@gmail.com>
2019-10-21 17:48:39 +0300

    Логотип и номер телефона в шапке сайта

--- js/rent.js
+++ js/rent.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   let premise = $('.premise');
   let rent = $('#rent');
   let close = $('#close');
-  let rentLink = $('.rent__link');
 
   premise.on('click', function(){
     rent.addClass('rent_active');
@@ -13,9 +12,6 @@ $(document).ready(function(){
     let target = $(this).attr('data-target');
     $('.rent-wrap').load(target+".html");
   });
-  // price.on('click', function(){
-  //   modal.addClass('modal_active');
-  // });
   
   close.on('click', function(){
     rent.removeClass('rent_active');
