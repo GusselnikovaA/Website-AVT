84de1adaa59f5766e3b7fb5209afa2126a31b166
GusselnikovaA <annanac1993@gmail.com>
2019-11-21 12:50:41 +0300

    Загрузка контента

--- src/js/rent.js
+++ src/js/rent.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-  var premise = $('.premise');
-  var rent = $('#rent');
-  var close = $('#close');
+// $(document).ready(function(){
+//   var premise = $('.premise');
+//   var rent = $('#rent');
+//   var close = $('#close');
 
-  premise.on('click', function(){
-    $('#up').hide('slow');
-    rent.addClass('rent_active');
-    var target = $(this).attr('data-target');
-    $.ajax({  
-      url: target+".html",  
-      cache: false,  
-      success: function(html){  
-          $(".rent-wrap").html(html);  
-          $('.rent-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 2000,
-            prevArrow: $('.rent__arrows__left'),
-            nextArrow: $('.rent__arrows__right'),
-          });
-      }
-    });  
+//   premise.on('click', function(){
+//     $('#up').hide('slow');
+//     rent.addClass('rent_active');
+//     var target = $(this).attr('data-target');
+//     $.ajax({  
+//       url: target+".html",  
+//       cache: false,  
+//       success: function(html){  
+//           $(".rent-wrap").html(html);  
+//           $('.rent-slider').slick({
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             speed: 2000,
+//             prevArrow: $('.rent__arrows__left'),
+//             nextArrow: $('.rent__arrows__right'),
+//           });
+//       }
+//     });  
     // var target = $(this).attr('data-target');
     // $('.rent-wrap').load(target+".html");
     // location.reload();    //JavaScript функция перегрузки страницы
@@ -30,7 +30,34 @@ $(document).ready(function(){
     //   $(".rent-slider").slick('setPosition');
     // else
     //   initSlider(".rent-slider");
-  });
+  // });
+
+  $(document).ready(function(){
+    var premise = $('.premise');
+    var rent = $('#rent');
+    var close = $('#close');
+  
+    $(document).on('click', '.premise', function(){
+      $('#up').hide('slow');
+      rent.addClass('rent_active');
+      $(".rent-slider").slick('reinit');
+      var target = $(this).attr('data-target');
+      $.ajax({  
+        url: "rent/"+target+".html",  
+        cache: false,  
+        success: function(html){  
+            $(".rent-wrap").html(html);  
+            var rentSlider = $('.rent-slider').slick({
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              speed: 2000,
+              prevArrow: $('.rent__arrows__left'),
+              nextArrow: $('.rent__arrows__right'),
+            });
+        rentSlider.slick('reinit');
+        }
+      });  
+    });
   
   close.on('click', function(){
     rent.removeClass('rent_active');
