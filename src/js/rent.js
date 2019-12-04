2ae9488fe80be2f7f53c6fff55497163fd63327f
GusselnikovaA <annanac1993@gmail.com>
2019-12-04 12:39:20 +0300

    Попытки починить слайдер

--- src/js/rent.js
+++ src/js/rent.js
@@ -38,33 +38,48 @@
     var close = $('#close');
   
     premise.on('click', function(){
-      $('#up').hide('slow');
-      rent.addClass('rent_active');
-      $(".rent-slider").slick('reinit');
+      $('#up').hide('slow'); 
+      // rent.addClass('rent_active');
       var target = $(this).attr('data-target');
-      $.ajax({  
-        url: "rent/"+target+".html",  
-        cache: false,  
-        success: function(html){  
-            $(".rent-wrap").html(html);  
-            $('.rent-slider').slick({
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              speed: 2000,
-              accessibility: true,
-              prevArrow: $('.rent__arrows__left'),
-              nextArrow: $('.rent__arrows__right'),
-            });
-            $('.rent-slider').slick('reinit');
-        }
-      });  
+      $(".rent-wrap").load("rent/"+target+".html", function() {
+        $('.rent-slider').slick({
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  speed: 2000,
+                  accessibility: true,
+                  prevArrow: $('.rent__arrows__left'),
+                  nextArrow: $('.rent__arrows__right'),
+                });
+      }, rent.addClass('rent_active'));
+      // Другой аналогичный вариант подгрузки
+      // $.ajax({  
+      //   url: "rent/"+target+".html",  
+      //   cache: false,  
+      //   success: function(html){  
+      //       $(".rent-wrap").html(html); 
+            // $('.rent-slider').slick('unslick'); 
+            // $('.rent-slider').slick({
+            //   slidesToShow: 1,
+            //   slidesToScroll: 1,
+            //   speed: 2000,
+            //   accessibility: true,
+            //   prevArrow: $('.rent__arrows__left'),
+            //   nextArrow: $('.rent__arrows__right'),
+            // });
+            // $('.rent-slider').slick('reinit');
+      //   }
+      // });  
+      
     });
+
   
+  // Закрывает модальное окно
   close.on('click', function(){
     rent.removeClass('rent_active');
     $('#up').show('slow');
   });
 
+  // Функция для закрытия модального окна при клике на свободное пространство
   $(document).ready(function(){
     rent.click(function(e){
       if( ! $(e.target).closest(".rent-container").length)
@@ -72,3 +87,4 @@
     });
   });
 });
+
